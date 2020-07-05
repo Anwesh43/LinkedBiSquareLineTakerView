b04e595778539e33fae948a8f738f4c6282ca162
Anwesh43 <anweshthecool0@gmail.com>
2020-07-06 04:17:17 +0530

    adding BSLTNode class

--- bisquarelinetakerview/src/main/java/com/anwesh/uiprojects/bisquarelinetakerview/BiSquareLineTakerView.kt
+++ bisquarelinetakerview/src/main/java/com/anwesh/uiprojects/bisquarelinetakerview/BiSquareLineTakerView.kt
@@ -124,4 +124,45 @@ class BiSquareLineTakerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSLTNode(var i : Int, val state : State = State()) {
+
+        private var next : BSLTNode? = null
+        private var prev : BSLTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSLTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSLTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSLTNode {
+            var curr : BSLTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
