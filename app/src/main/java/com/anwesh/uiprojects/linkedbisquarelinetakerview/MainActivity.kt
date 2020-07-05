2247a35f74b0954b9a472a0fa13ccd58f54d9614
Anwesh43 <anweshthecool0@gmail.com>
2020-07-06 04:31:59 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedbisquarelinetakerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbisquarelinetakerview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbisquarelinetakerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.bisquarelinetakerview.BiSquareLineTakerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiSquareLineTakerView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
